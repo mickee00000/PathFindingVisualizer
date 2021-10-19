a534b778b212ff9f72895844f55d454e91dac0d1
Manjot <mickee00000@gmail.com>
2021-10-20 01:57:19 +0530

    Changelog:
    -> Completed Dijkstra Steps...
    -> Rest of the unvisited nodes sorted by distance.
    -> Starting Node's distance set to zero.
    -> Converted Grid to Array of Objects.
    -> Passing Objects in the this.state grid variable with various properties.
    -> First Grid made using CSS square 20 X 50

--- src/algorithms/dijkstra.js
+++ src/algorithms/dijkstra.js
@@ -1,11 +1,54 @@
 function dijkshtra(grid, startNode, finishNode){
-    const visitedNodesinOrder = []; // Array to be Returned..
+    const visitedNodesInOrder = []; // Array to be Returned..
     startNode.distance = 0;
     const unvisiteNodes = twoD_array_to_oneD(grid);
 
     while(!!unvisiteNodes.length){
         sortNodesByDistance(unvisiteNodes);
+        const closestNode = unvisiteNodes.shift();
+        if (closestNode.isWall){
+            continue;
+        }
+        if (closestNode.distance === Infinity){
+            return visitedNodesInOrder;
+        }
+
+        closestNode.isVisited = true;
+        visitedNodesInOrder.push(closestNode);
+
+        if (closestNode === finishNode){
+            return visitedNodesInOrder;
+        }
+
+        updateUnvisitedNeighbour(closestNode, grid);
     }
+}
+
+function updateUnvisitedNeighbour(node, grid){
+    const unvisitedNeighbors = findUnvisitedNeighbours(node, grid);
+    for (const neighbour in unvisitedNeighbors){
+        neighbour.distance = node.distance + 1;
+        neighbour.previousNode = node;
+    }
+}
+
+function findUnvisitedNeighbours(node, grid){
+    const neighbours = [];
+    const {row, col} = node
+
+    if (row > 0)
+        neighbours.push(grid[row - 1][col])  // Upper neighbour
+
+    if (row < grid.length - 1)
+        neighbours.push(grid[row + 1][col])  // Bottom neighbour
+
+    if (col > 0)
+        neighbours.push(grid[row][col - 1])  // Left Neighbour
+
+    if (col < grid[0].length - 1)
+        neighbours.push(grid[row][col + 1])  // Right Neighbour
+
+    return neighbours.filter(neighbour => !neighbour.isVisited);
 
 }
 
